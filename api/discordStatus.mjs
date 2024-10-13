85c7ab7895bc235e63c12f914ef2807770353b38
BJS5705 <103567492+BJS5705@users.noreply.github.com>
2024-10-13 15:15:29 +0900

    Update discordStatus.mjs

--- api/discordStatus.mjs
+++ api/discordStatus.mjs
@@ -37,7 +37,6 @@ export default async (req, res) => {
     const guildId = '1192087206219763753'; // 확인할 Discord 서버 ID
     const userId = '332383283470139393'; // 확인할 Discord 사용자 ID
 
-    // 봇이 준비 완료 후 사용자 상태를 가져옵니다.
     if (!client.isReady()) {
         return res.status(503).json({ error: 'Bot is not ready' }); // 봇이 준비되지 않은 경우
     }
