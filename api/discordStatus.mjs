00ba2f586ccd8cfe00dc1c873b079aea462804c4
BJS5705 <103567492+BJS5705@users.noreply.github.com>
2024-10-13 14:46:35 +0900

    Update discordStatus.mjs

--- api/discordStatus.mjs
+++ api/discordStatus.mjs
@@ -37,17 +37,20 @@ export default async (req, res) => {
     const guildId = '1192087206219763753'; // 확인할 Discord 서버 ID
     const userId = '332383283470139393'; // 확인할 Discord 사용자 ID
 
-    try {
-        const status = await getUserStatus(guildId, userId); // 사용자 상태 가져오기
-        if (status) {
-            res.status(200).json({ status: status });
-        } else {
-            res.status(404).json({ error: 'User not found or no presence information available' });
+    // 봇이 준비 완료 후 사용자 상태를 가져옵니다.
+    client.once('ready', async () => {
+        try {
+            const status = await getUserStatus(guildId, userId); // 사용자 상태 가져오기
+            if (status) {
+                res.status(200).json({ status: status });
+            } else {
+                res.status(404).json({ error: 'User not found or no presence information available' });
+            }
+        } catch (error) {
+            console.error('Error fetching user status:', error);
+            res.status(500).json({ error: 'Failed to fetch user status' });
         }
-    } catch (error) {
-        console.error('Error fetching user status:', error);
-        res.status(500).json({ error: 'Failed to fetch user status' });
-    }
+    });
 };
 
 // 봇 로그인
